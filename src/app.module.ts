f68c0360d5253ec5dc379ba63c0ab82d8f0415da
Mohanad Ahmed <mhndahmd031@gmail.com>
2024-06-18 13:20:41 +0300

    generate needed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CartModule, OrdersModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
