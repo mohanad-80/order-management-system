e729edcd8b1bdf0eceb1ce7e60e5e8a8ba09b8dc
Mohanad Ahmed <mhndahmd031@gmail.com>
2024-06-20 05:08:10 +0300

    add minimum and examples for the apply coupon dto

--- src/orders/dtos/apply-coupon.dto.ts
+++ src/orders/dtos/apply-coupon.dto.ts
@@ -1,14 +1,15 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, IsPositive, Min, IsDecimal } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsPositive, Min, IsDecimal, Max } from 'class-validator';
 
 export class ApplyCouponDto {
-  @ApiProperty()
+  @ApiProperty({ minimum: 1 })
   @IsInt()
   @IsPositive()
   orderId: number;
-  
-  @ApiProperty()
+
+  @ApiProperty({ example: 0.33, minimum: 0, maximum: 1 })
   @IsDecimal()
   @Min(0)
+  @Max(1)
   discount: number;
 }
